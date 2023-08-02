1a7730a9f850b4bc161d4a39ffc12b077cf90592
Alex Vargas <alexvargas561@gmail.com>
2023-08-01 20:27:12 -0700

    Handled using the image picker

--- App.js
+++ App.js
@@ -2,15 +2,34 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import ImageViewer from './components/ImageViewer';
 import Button from './components/Button';
+import * as ImagePicker from 'expo-image-picker';
+import { useState } from 'react';
 const PlaceholderImage = require('./assets/images/background-image.png');
+
 export default function App() {
+  const [selectedImage,setSelectedImage] = useState(null);
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1
+    })
+    if(!result.canceled){
+      setSelectedImage(result.assets[0].uri);
+    }else{
+      alert('You did not pick an image');
+    }
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer placeholderImageSource={PlaceholderImage}></ImageViewer>
+        <ImageViewer 
+          placeholderImageSource={PlaceholderImage}
+          selectedImage={selectedImage}
+        ></ImageViewer>
       </View>
       <View style={styles.footerContainer}>
-        <Button label='Choose a photo'></Button>
+        <Button theme="primary" label='Choose a photo' onPress={pickImageAsync}></Button>
         <Button label='Use This Photo'></Button>
       </View>
       <StatusBar style="auto" />
